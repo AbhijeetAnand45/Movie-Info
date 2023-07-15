53cfcb8b7b38259d3f9767861dc93ac1a8af0d39
Abhijeet Anand <aabhijeet011@gmail.com>
2023-07-15 21:12:48 +0530

    u3

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const resultGrid = document.getElementById('result-grid');
 
 // load movies from API
 async function loadMovies(searchTerm){
-    //let apikey = config.API_KEY;
+    
     let alphakey = "451192ea"
     const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=${alphakey}`;
     const res = await fetch(`${URL}`);
